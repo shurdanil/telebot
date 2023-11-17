e1e8415f7d8c6a91a7b8839a7c98173e8f116a66
Alexander Danilov <a.danilov@mindsw.io>
2023-11-18 01:15:50 +0300

    second commit

--- main.go
+++ main.go
@@ -4,15 +4,15 @@ import (
 	"bufio"
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
-
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	g "github.com/novalagung/gubrak/v2"
 )
 
 type userModel struct {
@@ -21,6 +21,7 @@ type userModel struct {
 	Login        string
 	Password     string
 	Token        string
+	PersonId     int
 }
 
 var (
@@ -28,8 +29,7 @@ var (
 	firstMenu = "<b>Меню</b>\n"
 
 	portalButton = "Portal"
-
-	userDB map[int64]userModel
+	userDB       map[int64]userModel
 
 	bot *tgbotapi.BotAPI
 
@@ -137,17 +137,18 @@ func handleMessage(message *tgbotapi.Message) {
 		msg := tgbotapi.NewMessage(message.Chat.ID, "Спасибо)")
 		_, err = bot.Send(msg)
 
-		posturl := "https://userapi.riichimahjong.org/v2/common.Frey/Authorize"
+		authUrl := "https://userapi.riichimahjong.org/v2/common.Frey/Authorize"
 
-		body := []byte("\n\u0011" + userObject.Login + "\u0012\f" + userObject.Password)
+		var body = []byte(`{"email":"` + userDB[message.Chat.ID].Login + `", "password": "` + userDB[message.Chat.ID].Password + `"}`)
 
-		r, err := http.NewRequest("POST", posturl, bytes.NewBuffer(body))
+		r, err := http.NewRequest("POST", authUrl, bytes.NewBuffer(body))
 		if err != nil {
 			panic(err)
 		}
 
 		r.Header.Add("authority", "userapi.riichimahjong.org")
-		r.Header.Add("content-type", "application/protobuf")
+		r.Header.Add("accept", "application/json")
+		r.Header.Add("Content-Type", "application/json")
 
 		client := &http.Client{}
 		res, err := client.Do(r)
@@ -155,27 +156,167 @@ func handleMessage(message *tgbotapi.Message) {
 			panic(err)
 		}
 
-		fmt.Println(144, res.StatusCode)
 		if err != nil {
-			fmt.Println(err.Error())
+			fmt.Println("Error", err.Error())
 			msg := tgbotapi.NewMessage(message.Chat.ID, "Запрос вернул ошибку :(")
 			_, err = bot.Send(msg)
 		} else if res.StatusCode != 200 {
-			msg := tgbotapi.NewMessage(message.Chat.ID, "Что-то не так с логином или паролем :(\nВыполните повторно команду /login")
-			_, err = bot.Send(msg)
-		} else {
 			b, err := io.ReadAll(res.Body)
 			if err != nil {
 				log.Fatalln(err)
 			}
+			fmt.Println("Error", string(b))
+			msg := tgbotapi.NewMessage(message.Chat.ID, "Что-то не так с логином или паролем :(\nВыполните повторно команду /login")
+			_, err = bot.Send(msg)
+		} else {
 
-			token := g.From(strings.Split(string(b), "")).Drop(5).Result()
-			fmt.Println(168, token)
-			if token != nil {
-				fmt.Println(147, strings.Join(token.([]string), ""))
+			type target struct {
+				PersonId  int    `json:"personId"`
+				AuthToken string `json:"authToken"`
 			}
+
+			var data target
+			err = json.NewDecoder(res.Body).Decode(&data)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			err = r.Body.Close()
+			if err != nil {
+				return
+			}
+
+			fmt.Println(168, data.AuthToken)
+			userObject, ok := userDB[message.Chat.ID]
+			if ok {
+				userObject.Token = data.AuthToken
+				userObject.PersonId = data.PersonId
+				userDB[message.Chat.ID] = userObject
+			}
+
+			eventsUrl := "https://gameapi.riichimahjong.org/v2/common.Mimir/GetMyEvents"
+
+			body = []byte{}
+
+			r, err := http.NewRequest("POST", eventsUrl, bytes.NewBuffer(body))
+			if err != nil {
+				panic(err)
+			}
+
+			r.Header.Add("authority", "userapi.riichimahjong.org")
+			r.Header.Add("accept", "application/json; charset=UTF-8';")
+			r.Header.Add("Content-Type", "application/json; charset=UTF-8';")
+			r.Header.Add("x-auth-token", userDB[message.Chat.ID].Token)
+			r.Header.Add("x-current-person-id", strconv.Itoa(userDB[message.Chat.ID].PersonId))
+
+			client := &http.Client{}
+			res, err = client.Do(r)
+			if err != nil {
+				panic(err)
+			}
+			b, err := io.ReadAll(res.Body)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			fmt.Println(217, string(b))
+
+			type event struct {
+				Id          int    `json:"id"`
+				Title       string `json:"title"`
+				Description string `json:"description"`
+			}
+
+			type eventType struct {
+				Events []event `json:"events"`
+			}
+
+			var events eventType
+			err = json.Unmarshal(b, &events)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			fmt.Println(228, events)
+
+			var eventButtons [][]tgbotapi.InlineKeyboardButton
+
+			for _, e := range events.Events {
+				eventButtons = append(eventButtons, tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData(e.Title, strings.Join([]string{
+						"selectEvent",
+						strconv.Itoa(e.Id),
+						e.Title,
+					}, "|")),
+				))
+			}
+			eventsMenu := tgbotapi.NewInlineKeyboardMarkup(
+				eventButtons...,
+			)
+
+			msg := tgbotapi.NewMessage(message.Chat.ID, "Выберите событие")
+			msg.ParseMode = tgbotapi.ModeHTML
+			msg.ReplyMarkup = eventsMenu
+			_, err = bot.Send(msg)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			//fetch("https://userapi.riichimahjong.org/v2/common.Frey/QuickAuthorize", {
+			//	"headers": {
+			//		"accept": "application/protobuf",
+			//			"accept-language": "en-US,en;q=0.9,ru;q=0.8",
+			//			"content-type": "application/protobuf",
+			//			"sec-ch-ua": "\"Google Chrome\";v=\"119\", \"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\"",
+			//			"sec-ch-ua-mobile": "?0",
+			//			"sec-ch-ua-platform": "\"Linux\"",
+			//			"sec-fetch-dest": "empty",
+			//			"sec-fetch-mode": "cors",
+			//			"sec-fetch-site": "same-site",
+			//			"x-auth-token": "00e66e9481dc0dfe0d8da4b03b953d769098f22a4fdbe04ea836fb93ed1afb263f1513516f8b6881a3db43c708543fc8",
+			//			"x-current-person-id": "956",
+			//			"x-twirp": "true",
+			//			"Referer": "https://assist.riichimahjong.org/",
+			//			"Referrer-Policy": "strict-origin-when-cross-origin"
+			//	},
+			//	"body": "\b¼\u0007\u0012`00e66e9481dc0dfe0d8da4b03b953d769098f22a4fdbe04ea836fb93ed1afb263f1513516f8b6881a3db43c708543fc8",
+			//		"method": "POST"
+			//});
+
+			//quickAuthUrl := "https://userapi.riichimahjong.org/v2/common.Frey/AuthAuthorize"
+			//
+			//r, err = http.NewRequest("POST", quickAuthUrl, bytes.NewBuffer(jsonStr))
+			//if err != nil {
+			//	panic(err)
+			//}
+			//
+			//r.Header.Add("accept", "application/json")
+			//r.Header.Add("Content-Type", "application/json")
+			//r.Header.Add("x-auth-token", userDB[message.Chat.ID].Token)
+			//
+			//client := &http.Client{}
+			//res, err := client.Do(r)
+			//if err != nil {
+			//	panic(err)
+			//}
+			//
+			//fmt.Println(225, res)
+			//fmt.Println(r, res)
+			//for _, cookie := range res.Cookies() {
+			//	fmt.Println("Found a cookie named:", cookie.Name)
+			//}
+			//
+			//b, err = io.ReadAll(res.Body)
+			//if err != nil {
+			//	log.Fatalln(err)
+			//}
+			//fmt.Println(262, string(b))
+			//fmt.Println(262, binary.BigEndian.Uint16(b))
+
 		}
 		fmt.Println(130, userDB[message.Chat.ID])
+
+		//msg := tgbotapi.NewMessage(message.Chat.ID, firstMenu)
+		//msg.ParseMode = tgbotapi.ModeHTML
+		//msg.ReplyMarkup = firstMenuMarkup
+		//_, err := bot.Send(msg)
 	} else {
 		// This is equivalent to forwarding, without the sender's name
 		copyMsg := tgbotapi.NewCopyMessage(message.Chat.ID, message.Chat.ID, message.MessageID)
@@ -206,30 +347,33 @@ func handleCommand(chatId int64, command string) error {
 func handleButton(query *tgbotapi.CallbackQuery) {
 	var text string
 
-	markup := tgbotapi.NewInlineKeyboardMarkup()
 	message := query.Message
-	//
-	//if query.Data == nextButton {
-	//	text = secondMenu
-	//	markup = secondMenuMarkup
-	//} else if query.Data == backButton {
-	//	text = firstMenu
-	//	markup = firstMenuMarkup
-	//}
+
+	fmt.Println(352, query)
+	if strings.Contains(query.Data, "selectEvent") {
+		textList := strings.Split(query.Data, "|")
+		text = "Выбрано событие: " + textList[2]
+	}
 
 	callbackCfg := tgbotapi.NewCallback(query.ID, "")
 	bot.Send(callbackCfg)
 
-	// Replace menu text and keyboard
-	msg := tgbotapi.NewEditMessageTextAndMarkup(message.Chat.ID, message.MessageID, text, markup)
-	msg.ParseMode = tgbotapi.ModeHTML
+	msg := tgbotapi.NewMessage(message.Chat.ID, text)
 	bot.Send(msg)
+
 }
 
 func sendMenu(chatId int64) error {
 	msg := tgbotapi.NewMessage(chatId, firstMenu)
 	msg.ParseMode = tgbotapi.ModeHTML
 	msg.ReplyMarkup = firstMenuMarkup
+	_, err := bot.Send(msg)
+	return err
+}
+
+func selectEvent(chatId int64) error {
+	fmt.Println(367, chatId)
+	msg := tgbotapi.NewMessage(chatId, "Событие выбрано")
 	_, err := bot.Send(msg)
 	return err
 }
